c02a315614344c619bec8fc9a12d84c40890505c
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2024-10-12 23:20:53 +0800

    Feign 初始化

--- eem-2-admin/src/main/java/org/shop/AdminApplication.java
+++ eem-2-admin/src/main/java/org/shop/AdminApplication.java
@@ -4,6 +4,7 @@ package org.shop;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -14,6 +15,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling //开启定时任务
 @EnableAspectJAutoProxy()
+@EnableFeignClients()
 public class AdminApplication {
 
     public static void main(String[] args) {
