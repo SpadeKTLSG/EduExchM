222e6deb51a1ffaf9fbd800e501565e8789164dc
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2024-10-15 05:40:56 +0800

    all up test

--- EEM-gateway/src/main/java/org/shop/gateway/config/FeignConfig.java
+++ EEM-gateway/src/main/java/org/shop/gateway/config/FeignConfig.java
@@ -5,9 +5,6 @@ import feign.okhttp.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * Feign配置
- */
 
 /**
  * Feign配置
